89970b645eb685cbbc8f72943923a402c56c2a61
onielrosario <onielrosario@pursuit.org>
2019-02-08 10:54:54 -0500

    first commit

---
+++ SecondGitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondGitExample
+//
+//  Created by Oniel Rosario on 2/8/19.
+//  Copyright © 2019 Oniel Rosario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       print("working on git")
+    }
+
+
+}
+
